72a2cb4e673f144d3229a60248da89bb00cd1abd
MinhHangNguyen <minhhang151193@gmail.com>
2014-07-17 17:45:57 +0700

    commit

--- tab/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render_to_response , RequestContext
-
-def my_view(request):
-    return render_to_response('demo.html', locals(), context_instance = RequestContext(request))
-
